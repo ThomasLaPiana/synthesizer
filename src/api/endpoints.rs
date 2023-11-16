76fe9dd073ec6b5c9cbec80aca7165b126e226af
Thomas La Piana <tlapiana@pm.me>
2023-11-16 21:59:59 +0800

    add an endpoint that lists all endpoints

--- src/api/endpoints.rs
+++ src/api/endpoints.rs
@@ -1,15 +1,28 @@
 use axum::response::Response;
-use axum::routing::get;
+use axum::routing::{get, method_routing::MethodRouter};
 use axum::{Json, Router};
 use serde_json::{json, Value};
+use std::collections::HashMap;
+
+pub fn get_endpoint_map() -> HashMap<String, MethodRouter> {
+    let mut endpoints = HashMap::new();
+
+    endpoints.insert("/".to_owned(), get(root));
+    endpoints.insert("/api/health".to_owned(), get(health));
+    endpoints.insert("/api/pipelines".to_owned(), get(list_pipelines));
+    endpoints.insert("/api/endpoints".to_owned(), get(list_endpoints));
+
+    endpoints
+}
 
 /// Create a router with all of the endpoints used by the Games service
 pub fn create_pipeline_router() -> Router {
-    Router::new()
-        .route("/", get(root))
-        .route("/api/health", get(health))
-        .route("/api/pipelines", get(list_pipelines))
-        .route("/api/endpoints", get(list_endpoints))
+    let mut router = Router::new();
+
+    for (path, handler) in get_endpoint_map() {
+        router = router.route(&path, handler);
+    }
+    router
 }
 
 async fn root() -> Json<Value> {
@@ -26,9 +39,12 @@ async fn health() -> Json<Value> {
 
 /// Show all of the available routes for the server
 async fn list_endpoints() -> Json<Value> {
-    let router = create_pipeline_router();
+    let paths: Vec<String> = get_endpoint_map()
+        .keys()
+        .map(|path| path.to_owned())
+        .collect();
 
-    Json(json!({"endpoints": "Some stuff"}))
+    Json(json!({"endpoint_paths": paths}))
 }
 
 /// Return a list of all pipelines
