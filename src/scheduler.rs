c1896d1c7189f4a22e870c6a6d58f7c50196bd83
Thomas La Piana <tlapiana@pm.me>
2023-11-19 11:13:29 +0800

    checkin: start splitting things apart

---
+++ src/scheduler.rs
@@ -0,0 +1,5 @@
+// TODO
+// This should be a single process that is responsible for the following:
+// 1. Seeing that something is ready to be run in the database
+// 2. Spawning a thread to handle execution
+// 3. Checking the status?
